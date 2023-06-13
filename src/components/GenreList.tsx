d3ea4e84c960eafeaf1d9efb1fcccffceaa8c1bc
Boubacar <timera.boubacar@gmail.com>
2023-06-13 17:12:08 +0200

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,28 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/api-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
-      {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
-      ))}
-    </ul>
+    <List>
+      {data.map((genre) => {
+        return (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              {" "}
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                width="32px"
+                borderRadius={8}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        );
+      })}
+    </List>
   );
 };
 
