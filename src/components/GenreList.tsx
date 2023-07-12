ed5712c254da29dedd15553fa6d0258a46e989a5
Boubacar <timera.boubacar@gmail.com>
2023-07-12 13:07:20 +0200

    Filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,34 +1,51 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/api-url";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
   if (isLoading) return <Spinner />;
+
   return (
     <List>
-      {data.map((genre) => {
-        return (
-          <ListItem key={genre.id} paddingY="5px">
-            <HStack>
-              {" "}
-              <Image
-                src={getCroppedImageUrl(genre.image_background)}
-                width="32px"
-                borderRadius={8}
-              />
-              <Text fontSize="lg">{genre.name}</Text>
-            </HStack>
-          </ListItem>
-        );
-      })}
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            {" "}
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              width="32px"
+              borderRadius={8}
+            />
+            <Button
+              fontSize="lg"
+              variant="link"
+              onClick={() => onSelectGenre(genre)}
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
     </List>
   );
 };
 
 export default GenreList;
-function useGenre(): { data: any; error: any; isLoading: any } {
-  throw new Error("Function not implemented.");
-}
+// function useGenre(): { data: any; error: any; isLoading: any } {
+//   throw new Error("Function not implemented.");
+// }
